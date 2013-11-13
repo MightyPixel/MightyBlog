f162aed6b52f118f6af79ea128de5452d8d84938
MightyPixel <ognyan_angelov@yahoo.com>
2013-11-14 01:37:57 +0200

    production settings update

--- mightyblog/mightyblog/settings/production.py
+++ mightyblog/mightyblog/settings/production.py
@@ -5,7 +5,7 @@ EMAIL_PORT = 1025
 
 ALLOWED_HOSTS = ['*']#['mightypixel.net']
 
-DEBUG = True
+DEBUG = False 
 TEMPLATE_DEBUG = DEBUG
 
 
@@ -13,5 +13,5 @@ WSGI_APPLICATION = 'mightyblog.wsgi.application'
 
 INTERNAL_IPS = ("127.0.0.1", )
 
-#import dj_database_url
-#DATABASES['default'] =  dj_database_url.config()
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
