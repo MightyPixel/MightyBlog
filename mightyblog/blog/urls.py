5f760ec8adb97fd4b2c6596fab40cad927b92f34
Ognyan Angelov <shambhala@Ognyans-MacBook-Pro.local>
2014-07-26 23:33:33 +0300

    Refinements in frontend

--- mightyblog/blog/urls.py
+++ mightyblog/blog/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns
+from django.conf.urls import url
 
 
 urlpatterns = patterns('blog.views',
-    (r'^$', 'home'),
+    url(r'^$', 'home', name='home'),
     (r'^post/(?P<post_id>\d+)/(?P<post_name>\w+)', 'article'),
-    (r'^project/(?P<project_id>\d+)/(?P<project_name>\w+)', 'project'),
+    url(r'^project/(?P<project_id>\d+)/(?P<project_name>\w+)', 'project'),
     (r'^category/(?P<category>\w+)', 'category'),
-    (r'^articles/$', 'articles'),
-    (r'^projects/$', 'projects'),
-    (r'^self/$', 'about'),
+    url(r'^articles/$', 'articles', name='articles'),
+    url(r'^projects/$', 'projects', name='projects'),
+    url(r'^self/$', 'about', name='about'),
 )
